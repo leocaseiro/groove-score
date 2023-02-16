0f5bb131c11aa91ba19614cb0d48de3fba114900
leocaseiro <leobok@gmail.com>
2023-02-16 21:01:17 +1100

    Svelte Kit skeleton app setup

---
+++ playwright.config.ts
@@ -0,0 +1,11 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+	webServer: {
+		command: 'yarn run build && yarn run preview',
+		port: 4173
+	},
+	testDir: 'tests'
+};
+
+export default config;
