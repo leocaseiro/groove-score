674373de5dc39bdc9b5c5e63938fe6ac7a64d1d2
leocaseiro <leobok@gmail.com>
2023-02-17 17:45:05 +1100

    feat(dark): add tooltip for dark theme button

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
+	import Tooltip, { Wrapper } from '@smui/tooltip';
 	import IconButton from '@smui/icon-button';
 
 	import { darkTheme } from '$stores';
@@ -11,10 +12,13 @@
 	<Row>
 		<Section>
 			<IconButton class="material-icons">menu</IconButton>
-			<Title>Standard</Title>
+			<Title>Groove Score</Title>
 		</Section>
 		<Section align="end" toolbar>
-			<IconButton on:click={darkTheme.toggle} class="material-icons" aria-label={$darkTheme ? 'enable Light Mode' : 'enable Dark Mode'}>{$darkTheme ? 'light_mode' : 'dark_mode'}</IconButton>
+			<Wrapper>
+				<IconButton on:click={darkTheme.toggle} class="material-icons" aria-label={$darkTheme ? 'enable Light Mode' : 'enable Dark Mode'}>{$darkTheme ? 'light_mode' : 'dark_mode'}</IconButton>
+				<Tooltip xPos="start">{$darkTheme ? 'enable Light Mode' : 'enable Dark Mode'}</Tooltip>
+			</Wrapper>
 			<!-- <IconButton class="material-icons" aria-label="Print this page">dark_mode</IconButton> -->
 			<!-- <IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton> -->
 		</Section>
@@ -23,27 +27,6 @@
 <AutoAdjust {topAppBar}>
 	<slot />
 </AutoAdjust>
-
-<!-- <script>
-	import { page } from '$app/stores';
-</script>
-
-<header>
-	<nav>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-		</ul>
-	</nav>
-</header>
-
-<style>
-</style>
- -->
 
 <style>
 	/* Hide everything above this component. */
