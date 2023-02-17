8eb070701458d3ca26a0d093576add11083ff07f
leocaseiro <leobok@gmail.com>
2023-02-17 13:39:18 +1100

    chore(Svelte): fix issues with sveltekit to run the app

---
+++ src/routes/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">About</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+</style>
