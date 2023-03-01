59a60b5467ae10c8614c970dbef71e40de3cdffb
leocaseiro <leobok@gmail.com>
2023-03-02 03:39:15 +1100

    feat(MapMidi): validation, and save data to indexedDB as SPA

--- src/routes/settings/map-midi/[slug]/+page.ts
+++ src/routes/settings/map-midi/[slug]/+page.ts
@@ -1,7 +1,18 @@
+import { liveQuery } from "dexie";
+
+import { midiDB } from "$stores";
+import { browser } from '$app/environment';
+
 import type { PageLoad } from './$types';
 
+export const csr = true;
+export const prerender = false;
+
 export const load = (({ params }) => {
+  const note = liveQuery(
+      () => browser ? midiDB.notes.get(params.slug) : undefined
+  );
   return {
-    sound: params.slug,
+    note
   };
 }) satisfies PageLoad;
--- src/routes/settings/map-midi/[slug]/MapMidiSettingsForm.svelte
+++ src/routes/settings/map-midi/[slug]/MapMidiSettingsForm.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
     import { createForm } from 'svelte-forms-lib';
     import { array, number, object } from 'yup';
+    import Button from '@smui/button';
     import IconButton from '@smui/icon-button';
     import Textfield from '@smui/textfield';
     import HelperText from '@smui/textfield/helper-text';
-    import Button from '@smui/button';
-
+    import Kitchen from '@smui/snackbar/kitchen';
 
     import { midiDB } from '$stores';
     import type { INote } from '$stores/midi/midiModel';
     import { mapArrToObjValues, mapObjValuesToArr } from '$lib/utils';
+
+    let success: Kitchen;
 
     export let note: INote;
 
@@ -22,30 +24,46 @@
         }),
         onSubmit: (values) => {
             const newMidi = mapObjValuesToArr<number>(values.midi).map(n => Number(n));
-            console.log({newMidi});
             midiDB.notes.update(note.sound, { midi: newMidi});
+
+            // Hack to fix bug with $errors not updating
+            // @ts-expect-error Type 'string' is not assignable to type 'number'.ts(2769)
+            $errors.midi = newMidi.map(() => ({ val: '' }));
+
+            success.push({
+                props: {
+                    variant: 'stacked',
+                },
+                label: `The MIDI for ${note.sound} was updated with ${newMidi.join(', ')}`,
+                dismissButton: true,
+            });
         }
     });
 
-    const addMidi = () => {
+    $: addMidi = () => {
         // @ts-expect-error Type 'string' is not assignable to type 'number'.ts(2769)
         $form.midi = $form.midi.concat({ val: '' });
         // @ts-expect-error Type 'string' is not assignable to type 'number'.ts(2769)
         $errors.midi = $errors.midi.concat({ val:  '' });
     }
-    const removeMidi = (index: number) => {
+
+    $: removeMidi = (index: number) => {
         // @ts-expect-error Property 'filter' does not exist on type 'string'.ts(2339)
         $errors.midi = $errors.midi.filter((item: { val: number }, i: number) => i !== index);
         $form.midi = $form.midi.filter((_, i: number) => i !== index);
     }
+
+    $: cast = (midi: any): { val: number } => midi;
 </script>
+
+<Kitchen bind:this={success} dismiss$class="material-icons" />
 
 <form on:submit={handleSubmit}>
     {#each $form.midi as midi, i}
     <IconButton on:click={(e) => { e.preventDefault(); removeMidi(i)}} class="material-icons" aria-label="Remove midi">remove</IconButton>
     <Textfield name={`midi[${i}].val`} label="Midi" bind:value={$form.midi[i].val}>
         <svelte:fragment slot="helper">
-            <HelperText persistent validationMsg>{$errors?.midi[i] && $errors.midi[i].val}</HelperText>
+            <HelperText persistent validationMsg>{$errors?.midi[i] && cast($errors.midi[i])?.val}</HelperText>
         </svelte:fragment>
     </Textfield>
     {/each}
